c278dea5d1ea9829c621b7ecda10e8e1570b87fa
Decagon <decagon@Decagons-MacBook-Air.local>
2021-04-04 23:00:00 +0100

    all test are passing

--- src/classifier.js
+++ src/classifier.js
@@ -4,6 +4,105 @@
  *
  * @param {any} input Array of student objects
  */
-function classifier(input) {}
+
+
+// get age of student
+ const ageOfStudent = (date) => {
+  const age = new Date().getFullYear() - new Date(date).getFullYear()
+  return age
+}
+
+// check groups before assigning a student
+const checkGroup = (student, groups ) => {
+  return groups.every((st) => {
+  if (ageOfStudent(st.dob) < ageOfStudent(student.dob)) {
+      return ageOfStudent(student.dob) - ageOfStudent(st.dob) <= 5
+  }
+      return ageOfStudent(st.dob) - ageOfStudent(student.dob) <= 5
+  })
+}
+
+// Create new groups function
+const createGroups = (studentArr) => {
+  let groups = []
+  groups.push(studentArr.splice(0, 1))
+
+
+  while(studentArr.length > 0) {
+      let bool = false
+      const student = studentArr.splice(0, 1)[0]
+      groups.forEach((group) => {
+          if (checkGroup(student, group) && group.length <= 2) {
+              group.push(student)
+              bool = true
+          }
+      })
+      if (!bool) {
+          groups.push([student])
+      }
+  }
+    return groups
+}
+
+
+function classifier(input) {
+
+  const studentArr = JSON.parse(JSON.stringify(input))
+
+  if (studentArr.length === 0) return { noOfGroups: 0 }
+
+  // Set age of students in group
+  for(let i = 0; i < studentArr.length; i++){
+    let date =  new Date().getFullYear() - new Date(studentArr[i].dob).getFullYear()
+    studentArr[i].age = date
+  }
+
+  // sort groups
+  studentArr.sort((a, b) => a.age - b.age)
+
+  let chunked = {}
+  let count = 0
+
+  // get the maximum age in a group
+  const max = (arr) => Math.max(...arr)
+
+  // get the total sum of ageSum
+  const ageSum = (arr) => arr.reduce((a, b) => { return a + b}, 0)
+
+  // create groups of students
+  const chonked = createGroups(studentArr)
+
+    // loop through each group and update member info
+  for(let i = 0; i < chonked.length; i++) {
+      let sortedMembers = []
+
+      chonked[i].forEach((grp) => {
+          sortedMembers.push({
+              name: grp.name,
+              age: grp.age,
+              dob: grp.dob,
+              regNo: grp.regNo
+          })
+      })
+
+      const allAges = sortedMembers.map((member) => member.age)
+      const sumOfAllAges = ageSum(allAges)
+      const regNos = chonked[i].map((member) => Number(member.regNo))
+      regNos.sort((a, b) => a - b)
+
+      count += 1
+
+      // update group info according to their group name
+      chunked[`group${count}`] = {
+          oldest: max(allAges),
+          members: sortedMembers,
+          sum: sumOfAllAges,
+          regNos
+      }
+  }
+  // update number of groups
+  chunked.noOfGroups = count
+  return chunked
+}
 
 module.exports = classifier;
--- src/laundry.js
+++ src/laundry.js
@@ -4,6 +4,216 @@
  *
  * @returns {any} Trip data analysis
  */
-function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {}
+
+// function to check sock pairs
+ const solution = (p, c) => {
+  let count = 0
+  let ab = p
+
+  if(p === 0 || c === 0){
+      return 0
+  }
+
+  for (i = 2; i <= p; i +=2 ){
+      count++
+         ab -= 2
+      if(count === c){
+          return count
+      }
+  }
+
+ return count
+
+}
+
+
+function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {
+
+  let count = noOfWashes
+  let cleanPileObj = {}
+  let newDirtyPile = [...dirtyPile]
+
+// convert cleanPile to objects
+  function convertToObj() {
+      cleanPileObj = cleanPile.reduce( (tally, fruit) => {
+          tally[fruit] = (tally[fruit] || 0) + 1 ;
+          return tally;
+      } , {})
+  }
+
+  // create dirtyPile Objects
+  dirtyPileObj = dirtyPile.reduce( (tally, fruit) => {
+      tally[fruit] = (tally[fruit] || 0) + 1 ;
+      return tally;
+  } , {})
+
+
+  // delete removed sock from Object
+  function deleteValue(key, obj, type = 1) {
+      obj[key] -= type
+  }
+
+  // function to call pair function and get total pair
+  function checkPair() {
+      let pair = 0
+      for (const key in cleanPileObj) {
+          pair += solution(cleanPileObj[key])
+      }
+
+      return pair
+  }
+
+  // function to check if property is in object
+  function checkOjects(value, obj) {
+      return !!obj[value]
+  }
+
+  // call the fuction to create cleanPile Object
+  convertToObj()
+
+  // calculate the total pair in cleanPile Objects if noOfWashes id 0
+  if (noOfWashes === 0) {
+      return checkPair()
+  }
+
+    // function to check properties in dirtyPile that has values greater than 0
+  function checkDirtyObj() {
+      const odd = []
+      for (const key in dirtyPileObj) {
+          if(dirtyPileObj[key] > 0 ) {
+              odd.push(key)
+          }
+      }
+
+      return odd.length > 0
+  }
+
+  // function to get a random sock from dirtyPile
+  function getRandomDirtyPile() {
+      const arr = []
+      for (const key in dirtyPileObj) {
+          if (dirtyPileObj[key] > 0) {
+              arr.push(key)
+          }
+      }
+
+      const value = arr.length > 0 ? arr[0] : 0
+      return Number(value)
+  }
+
+  // function to get a sock from clean pile that has an incomplete pair
+  function getCleanPile() {
+      const odd = []
+      for (const key in cleanPileObj) {
+          if(cleanPileObj[key] % 2 !== 0) {
+              odd.push(key)
+          }
+      }
+      const value = odd[0]
+      return Number(value)
+  }
+
+ // function to get one sock from cleanPile
+  function getOneClean() {
+      const odd = []
+      for (const key in cleanPileObj) {
+          if(cleanPileObj[key] === 1) {
+              odd.push(key)
+          }
+      }
+
+      const value = odd[0]
+      return Number(value)
+  }
+
+   // function to get one sock from dirtyPile
+  function getOneDirty() {
+      const one = []
+      for (const key in dirtyPileObj) {
+          if(dirtyPileObj[key] === 1) {
+              one.push(key)
+          }
+      }
+      const value = one[0]
+      return Number(value)
+  }
+
+  // function to get a pair at once from dirtyPile
+  function getTwoDirty() {
+      const arr = []
+      for (const key in dirtyPileObj) {
+          if(dirtyPileObj[key] % 2 === 0  && dirtyPileObj[key] > 0) {
+              arr.push(key)
+          } else if ( dirtyPileObj[key] > 2) {
+              cleanPileObj[key] ? cleanPileObj[key] += 2 : cleanPileObj[key] = 2
+              deleteValue(key, dirtyPileObj, 2)
+              count -= 2
+          }
+      }
+
+      const value = arr[0]
+      return Number(value)
+  }
+
+  function getGreaterValueInDirty(key) {
+      const f = Object.keys(cleanPileObj).filter((d) => {
+          return cleanPileObj[d] > cleanPileObj[key] && dirtyPileObj[d] > cleanPileObj[key]
+      } )
+      return Number(f[0])
+  }
+
+ for(let i = 0; i < newDirtyPile.length; i++ ) {
+     if (!checkDirtyObj() || count === 0) {
+         break;
+     }
+
+     const value =  getCleanPile()
+     if (checkOjects(Number(value), dirtyPileObj) && count > 0) {
+        cleanPileObj[value] ? cleanPileObj[value] += 1 : cleanPileObj[value] = 1
+          deleteValue(value, dirtyPileObj)
+          count -= 1
+      }
+
+     if ( count === 1) {
+          const oneValue = getOneClean()
+          if (dirtyPileObj[oneValue]) {
+              cleanPileObj[oneValue] += 1
+              deleteValue(oneValue, dirtyPileObj)
+              count -= 1
+          }
+      }
+
+     if (count === 2) {
+          let twoValue = getTwoDirty()
+          if (count > 0) {
+            if (twoValue) {
+              const type = 2
+              cleanPileObj[twoValue] ? cleanPileObj[twoValue] += 2 : cleanPileObj[twoValue] = 2
+              deleteValue(twoValue, dirtyPileObj, type)
+              count -= 2
+            } else {
+              let oneVal = getOneDirty()
+              cleanPileObj[oneVal] ? cleanPileObj[oneVal] += 1 : cleanPileObj[oneVal] = 1
+              deleteValue(oneVal, dirtyPileObj)
+              oneVal = getOneDirty()
+              cleanPileObj[oneVal] ? cleanPileObj[oneVal] += 1 : cleanPileObj[oneVal] = 1
+              deleteValue(oneVal, dirtyPileObj)
+              count -= 1
+            }
+          }
+     }
+
+     if (count > 2) {
+          const value = getRandomDirtyPile()
+          cleanPileObj[value] ? cleanPileObj[value] += 1 : cleanPileObj[value] = 1
+          deleteValue(value, dirtyPileObj)
+          count -= 1
+     }
+
+ }
+
+  return checkPair()
+
+}
 
 module.exports = getMaxPairs;
